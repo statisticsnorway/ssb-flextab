47ead16fae26b56e29e00e21367aa531f91e228a
ssb-mfm <mfm@ssb.no>
2026-08-27 13:27:57 +0200

    Defining public API Users can import flextab like this    from ssb_flextab import flextab

--- src/ssb_flextab/__init__.py
+++ src/ssb_flextab/__init__.py
@@ -1,1 +1,10 @@
-"""SSB flextab."""
+from .flextab import flextab
+from .formatting import flextab_to_markdown, flextab_to_string
+from .result import FlextabResult
+
+__all__ = [
+    "flextab",
+    "flextab_to_markdown",
+    "flextab_to_string",
+    "FlextabResult",
+]

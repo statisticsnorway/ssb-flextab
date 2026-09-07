34a8cec2a8bb133d2e088fd5b5aceeb7c0a47dad
Kristian Lønø <kristian.loenoe@gmail.com>
2026-09-07 13:21:30 +0200

    fix noxfile for github mypy and xdoctest

--- noxfile.py
+++ noxfile.py
@@ -22,18 +22,18 @@ except ImportError:
     {sys.executable} -m pip install nox-poetry"""
     raise SystemExit(dedent(message)) from None
 
-package = "ssb-flextab"
+package = "ssb_flextab"
 python_versions = ["3.13", "3.12", "3.14"]
 python_versions_for_test = python_versions
 nox.needs_version = ">= 2025.2.9"
-nox.options.sessions = (
+nox.options.sessions = [
     "pre-commit",
     "mypy",
     "tests",
     "typeguard",
     "xdoctest",
     "docs-build",
-)
+]
 
 
 def install_poetry_groups(session: Session, *groups: str) -> None:

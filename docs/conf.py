bcd8f93e586d1390d81cf062eca4657688eb7b69
Kristian Lønø <kristian.loenoe@gmail.com>
2026-09-09 16:23:12 +0200

    add annotation

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = []
+templates_path: list[str] = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
